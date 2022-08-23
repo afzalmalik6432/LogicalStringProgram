10482614fff11a3b5e03cf09bdb706d7197cd0bc
afzalmalik6432 <afzalmalik6432@gmail.com>
2022-08-23 23:15:43 +0530

    forthCommit

--- src/com/ashokIt/Program01.java
+++ src/com/ashokIt/Program01.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Program01 {
     public static void main(String[] args) {
 
-        // Approach 1 --> Using StringBuffer class
         System.out.println("Enter the String to reverse: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+
+        // Approach 1 --> Using StringBuffer class
         StringBuffer sb1 = new StringBuffer(str);
         System.out.println("Reversed String: "+sb1.reverse());
         sc.close();
+
+        // Approach 2 --> Using StringBuilder class
+        StringBuilder sb2 = new StringBuilder(str);
+        System.out.println("Reversed String: "+sb2.reverse());
     }
 }
