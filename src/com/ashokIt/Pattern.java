5a4d8d056ba95f361591f14572ec194f9ed8cdd5
Afzal Ahmad <afzalmalik6432@gmail.com>
2022-08-25 19:17:53 +0530

    nineCommit

--- src/com/ashokIt/Pattern.java
+++
@@ -1,8 +0,0 @@
-package com.ashokIt;
-
-public class Pattern {
-    public static void main(String[] args) {
-        String s1 = "COMPUTER";
-    String s2 = "LAPTOP";
-    }
-}
--- src/com/ashokIt/Program12.java
+++ src/com/ashokIt/Program12.java
@@ -7,11 +7,18 @@ public class Program12 {
         System.out.println("Enter the String to sort: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        char temp;
         for (int i = 0; i < str.length(); i++) {
             for (int j = i+1; j < str.length(); j++) {
-                
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
+        System.out.println("String after sorting the data: "+String.valueOf(arr));
         sc.close();
     }
 }
